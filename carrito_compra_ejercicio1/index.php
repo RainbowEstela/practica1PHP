1ed3fd9b89c7d344408362ae7a60533e6c0d0960
Estela <eloiseruiz15@gmail.com>
2023-09-29 20:06:29 +0200

    empezando practica 1 imagenes y header

--- carrito_compra_ejercicio1/index.php
+++ carrito_compra_ejercicio1/index.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
     <title>Compra</title>
 </head>
 <body>
+    <header class="opposite cblue p10">
+        <div><img src="./img/tienda.png" alt="logo" width="50px"></div>
+        <div class="flex_l"><input type="button" value="inicio"></div>
+    </header>
     
 </body>
 </html>
