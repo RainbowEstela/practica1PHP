c6771cc52cbe0fd8fd6668ea81692f2441eb30d3
Estela <eloiseruiz15@gmail.com>
2023-09-30 10:42:51 +0200

    mas cambios

--- carrito_compra_ejercicio1/index.php
+++ carrito_compra_ejercicio1/index.php
@@ -8,7 +8,10 @@
 </head>
 <body>
     <header class="opposite cblue p10">
-        <div><img src="./img/tienda.png" alt="logo" width="50px"></div>
+        <div class="flexl">
+            <div><img src="./img/tienda2.png" alt="logo" width="50px"></div>
+            <div><p>telestela</p></div>
+        </div>
         <div class="flex_l"><input type="button" value="inicio"></div>
     </header>
     
