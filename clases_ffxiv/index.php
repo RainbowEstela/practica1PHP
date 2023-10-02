245c152a700faec799c1295b7ff9563bdf35665e
Estela <eloiseruiz15@gmail.com>
2023-10-02 20:33:52 +0200

    esqueleto header ejer3 mas imagenes

---
+++ clases_ffxiv/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>final fantasy xiv - clases</title>
+</head>
+<body>
+    <header class="cgrey p20px">
+        <div class="opposite">
+            <div>
+                <img src="./img/logo.png" alt="" width="100px">
+            </div>
+            <div>
+                <form action="">
+                    <input type="text" placeholder="search...">
+                    <button type="submit">search</button>
+                </form>
+            </div>
+        </div>
+        <nav>
+            <ul class="flexl p0px nolistdec">
+                <li>paladin</li>
+                <li>warrior</li>
+                <li>white mage</li>
+                <li>scholar</li>
+                <li>machinist</li>
+                <li>summoner</li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>
