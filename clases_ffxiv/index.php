e507663bf4d0abbb07dd42ae61ed76fa35b2d30e
Estela <eloiseruiz15@gmail.com>
2023-10-05 11:32:05 +0200

    ejer3 header hecho falta nav sticky

--- clases_ffxiv/index.php
+++ clases_ffxiv/index.php
@@ -4,26 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/styles.css">
+    <link rel="shortcut icon" href="./img/favicon.png" type="image/x-icon">
     <title>final fantasy xiv - clases</title>
 </head>
 <body>
-    <header class="cgrey p20px opposite nomargin">
+    <header class="cgrey p20px opposite g20px nomargin">
+        <a href="./index.php" class="notextdec">
         <div class="flexlnowrap">
             <img src="./img/logo.png" alt="" width="100px">
             <p class="cblue nomargin">
                 Final Fantasy XIV
             </p>
         </div>
+        </a>
+        <div class="flexsolo wrap g20px">
+            <div class="flexsolo g10px">
+                <a href=""><img src="./img/spain.jpg" width="30px" height="30px" alt="" class="b50per"></a>
+                <a href=""><img src="./img/english.png" width="30px" height="30px" alt="" class="b50per"></a>
+            </div>
+            
 
-        <div>
-            <form action="">
-                <input type="text" placeholder="search...">
-                <button type="submit">search</button>
+            <form action="" class="flexsolo">
+                <input type="text" placeholder="search..." class="searchbardec p10px">
+                <button type="submit" class="searchbutdec p10px">search</button>
             </form>
         </div>
     </header>
     <nav>
-        <ul class="flexl p0px nolistdec cgrey nomargin">
+        <ul class="flexlgap5px p10px nolistdec cgrey nomargin">
             <li><a href="" class="cWhite">paladin</a></li>
             <li><a href="" class="cWhite">warrior</a></li>
             <li><a href="" class="cWhite">white mage</a></li>
