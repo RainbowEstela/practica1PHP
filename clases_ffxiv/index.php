6ccf18a4c6ec6f1a8d871b83f6dc56702bf2b928
Estela <eloiseruiz15@gmail.com>
2023-10-07 11:09:53 +0200

    ejer3 esqueleto terminado falta php y español

--- clases_ffxiv/index.php
+++ clases_ffxiv/index.php
@@ -45,7 +45,7 @@
         <div class="card bgsize alamingo">
             <div class="insidecard gridcard">
                 <div class="titulo">
-                    <h3 class="textcen">Paladin</h3>
+                    <h3 class="textcen fonttitulo">Paladin</h3>
                 </div>
 
                 <div class="presentacion oppositecol bpresen descsize p20px">
@@ -86,21 +86,265 @@
             </div>
         </div>
         <div class="card bgsize alamingocas">
-            hola
+            <div class="insidecard gridcardreverse">
+                    <div class="titulo">
+                        <h3 class="textcen fonttitulo">Paladin</h3>
+                    </div>
+
+                    <div class="presentacion oppositecol bpresen descsize p20px">
+                        <div>
+                            <p>Paladin is a tank based on meele damage and spells. it also has utility habilities to help your teammates.</p>
+                        </div>
+                        <div class="gridhabilitys">
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000156.png"  width="50px" alt="">
+                                    <p>hability 1</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000161.png" width="50px" alt="">
+                                    <p>hability 2</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000167.png" width="50px" alt="">
+                                    <p>hability 3</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002506.png" width="50px" alt="">
+                                    <p>hability 4</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002509.png" width="50px" alt="">
+                                    <p>hability with bigger name</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002515.png" width="50px" alt="">
+                                    <p>hability 6</p>
+                                </div>
+                        </div>
+                    </div>
+
+                    <div class="imgclase felxcen">
+                        <img src="./img/Paladin.webp" alt="">
+                    </div>
+                </div>
         </div>
         <div class="card bgsize azisteppe">
-            hola
+        <div class="insidecard gridcard">
+                <div class="titulo">
+                    <h3 class="textcen fonttitulo">Paladin</h3>
+                </div>
+
+                <div class="presentacion oppositecol bpresen descsize p20px">
+                    <div>
+                        <p>Paladin is a tank based on meele damage and spells. it also has utility habilities to help your teammates.</p>
+                    </div>
+                    <div class="gridhabilitys">
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/000156.png"  width="50px" alt="">
+                                <p>hability 1</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/000161.png" width="50px" alt="">
+                                <p>hability 2</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/000167.png" width="50px" alt="">
+                                <p>hability 3</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/002506.png" width="50px" alt="">
+                                <p>hability 4</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/002509.png" width="50px" alt="">
+                                <p>hability with bigger name</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/002515.png" width="50px" alt="">
+                                <p>hability 6</p>
+                            </div>
+                    </div>
+                </div>
+
+                <div class="imgclase felxcen">
+                    <img src="./img/Paladin.webp" alt="">
+                </div>
+            </div>
         </div>
         <div class="card bgsize limsalominsa">
-            hola
+        <div class="insidecard gridcardreverse">
+                    <div class="titulo">
+                        <h3 class="textcen fonttitulo">Paladin</h3>
+                    </div>
+
+                    <div class="presentacion oppositecol bpresen descsize p20px">
+                        <div>
+                            <p>Paladin is a tank based on meele damage and spells. it also has utility habilities to help your teammates.</p>
+                        </div>
+                        <div class="gridhabilitys">
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000156.png"  width="50px" alt="">
+                                    <p>hability 1</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000161.png" width="50px" alt="">
+                                    <p>hability 2</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000167.png" width="50px" alt="">
+                                    <p>hability 3</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002506.png" width="50px" alt="">
+                                    <p>hability 4</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002509.png" width="50px" alt="">
+                                    <p>hability with bigger name</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002515.png" width="50px" alt="">
+                                    <p>hability 6</p>
+                                </div>
+                        </div>
+                    </div>
+
+                    <div class="imgclase felxcen">
+                        <img src="./img/Paladin.webp" alt="">
+                    </div>
+                </div>
         </div>
         <div class="card bgsize mordhona">
-            hola
+        <div class="insidecard gridcard">
+                <div class="titulo">
+                    <h3 class="textcen fonttitulo">Paladin</h3>
+                </div>
+
+                <div class="presentacion oppositecol bpresen descsize p20px">
+                    <div>
+                        <p>Paladin is a tank based on meele damage and spells. it also has utility habilities to help your teammates.</p>
+                    </div>
+                    <div class="gridhabilitys">
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/000156.png"  width="50px" alt="">
+                                <p>hability 1</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/000161.png" width="50px" alt="">
+                                <p>hability 2</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/000167.png" width="50px" alt="">
+                                <p>hability 3</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/002506.png" width="50px" alt="">
+                                <p>hability 4</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/002509.png" width="50px" alt="">
+                                <p>hability with bigger name</p>
+                            </div>
+                            <div class="flexsolo g5px justaligcen">
+                                <img src="./img/habilitys/paladin/002515.png" width="50px" alt="">
+                                <p>hability 6</p>
+                            </div>
+                    </div>
+                </div>
+
+                <div class="imgclase felxcen">
+                    <img src="./img/Paladin.webp" alt="">
+                </div>
+            </div>
         </div>
         <div class="card bgsize gridania">
-            hola
+        <div class="insidecard gridcardreverse">
+                    <div class="titulo">
+                        <h3 class="textcen fonttitulo">Paladin</h3>
+                    </div>
+
+                    <div class="presentacion oppositecol bpresen descsize p20px">
+                        <div>
+                            <p>Paladin is a tank based on meele damage and spells. it also has utility habilities to help your teammates.</p>
+                        </div>
+                        <div class="gridhabilitys">
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000156.png"  width="50px" alt="">
+                                    <p>hability 1</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000161.png" width="50px" alt="">
+                                    <p>hability 2</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/000167.png" width="50px" alt="">
+                                    <p>hability 3</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002506.png" width="50px" alt="">
+                                    <p>hability 4</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002509.png" width="50px" alt="">
+                                    <p>hability with bigger name</p>
+                                </div>
+                                <div class="flexsolo g5px justaligcen">
+                                    <img src="./img/habilitys/paladin/002515.png" width="50px" alt="">
+                                    <p>hability 6</p>
+                                </div>
+                        </div>
+                    </div>
+
+                    <div class="imgclase felxcen">
+                        <img src="./img/Paladin.webp" alt="">
+                    </div>
+                </div>
         </div>
     </section>
+
+    <footer class="cgrey oppositecol g40px">
+        <div class="felxcen">
+            <div>
+                <h2 class="textcen textazulfoot">follow our social websites</h2>
+                <div class="felxcen redes g10px">
+                    <a href=""><img src="./img/redes/facebook.png" alt=""></a>
+                    <a href=""><img src="./img/redes/gorjeo.png" alt=""></a>
+                    <a href=""><img src="./img/redes/youtube.png" alt=""></a>
+                    <a href=""><img src="./img/redes/instagram.png" alt=""></a>
+                    <a href=""><img src="./img/redes/reddit.png" alt=""></a>
+                </div>
+            </div>
+            
+        </div>
+        <div>
+            <ul class="flexfooter nolistdec nomargin g40px p0px">
+                <li>
+                    <p class="textazulfoot">community</p>
+                    <ul class="nolistdec nomargin p0px">
+                        <li><a href="" class="cWhite">Our discord channel</a></li>
+                        <li><a href="" class="cWhite">Join the game</a></li>
+                        <li><a href="" class="cWhite">Share your ideas</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <p class="textazulfoot">support</p>
+                    <ul class="nolistdec nomargin p0px">
+                        <li><a href="" class="cWhite">Feedback</a></li>
+                        <li><a href="" class="cWhite">Site map</a></li>
+                        <li><a href="" class="cWhite">Contact us</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <p class="textazulfoot">account</p>
+                    <ul class="nolistdec nomargin p0px">
+                        <li><a href="" class="cWhite">Login</a></li>
+                        <li><a href="" class="cWhite">Registre</a></li>
+                        <li><a href="" class="cWhite">FAQ</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </footer>
     
 </body>
 </html>
